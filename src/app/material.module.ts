c7ab8fad868583ee2759b60231a5d2b486dedf6f
“inziani” <ValentineRobai.Inziani@gmail.com>
2022-03-04 19:14:13 +0300

    Project set up - Install bootstrap and Angular Material

---
+++ src/app/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, } from "@angular/core";
+
+import { MatSliderModule } from '@angular/material/slider';
+
+
+
+@NgModule({
+  declarations: [],
+  imports:
+    [ MatSliderModule,
+    ],
+  exports:
+    [ MatSliderModule
+
+  ]
+})
+
+export class MaterialModule{
+
+}
