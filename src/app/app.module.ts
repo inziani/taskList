d9b29e7e00f6f41701fb90621d492c0d9d903d06
“inziani” <ValentineRobai.Inziani@gmail.com>
2022-03-07 10:35:50 +0300

    Application components set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { LoginComponent } from './core/authentication/login/login.component';
+import { SignupComponent } from './core/authentication/signup/signup.component';
+import { HeaderNavComponent } from './core/shared/header-nav/header-nav.component';
+import { SideNavComponent } from './core/shared/side-nav/side-nav.component';
+import { HomePageComponent } from './application-components/home-page/home-page.component';
+import { CreateTaskComponent } from './application-components/create-task/create-task.component';
+import { ChangeTaskComponent } from './application-components/change-task/change-task.component';
+import { FooterComponent } from './core/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderNavComponent,
+    SideNavComponent,
+    HomePageComponent,
+    CreateTaskComponent,
+    ChangeTaskComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/application-components/change-task/change-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-task',
+  templateUrl: './change-task.component.html',
+  styleUrls: ['./change-task.component.css']
+})
+export class ChangeTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
