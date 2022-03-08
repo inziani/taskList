3e1817e71637c0bc6dec5a421e5343382cb5ea2e
“inziani” <ValentineRobai.Inziani@gmail.com>
2022-03-08 17:10:17 +0300

    Edit task

--- src/app/application-components/change-task/change-task.component.ts
+++ src/app/application-components/change-task/change-task.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RestDataSource } from 'src/app/core/shared/data/rest.datasource';
+import { ChangesSavedComponent } from 'src/app/core/shared/dialogues/changes-saved/changes-saved.component';
+import { TasksFormGroup } from 'src/app/core/shared/models/task-form-model';
+import { Tasks } from 'src/app/core/shared/models/task-model';
 
 @Component({
   selector: 'app-change-task',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangeTaskComponent implements OnInit {
 
-  constructor() { }
+  public formGroup = new TasksFormGroup();
+  public isLoading = false;
+  public formSubmitted: boolean = false;
+  public task!: Tasks;
+  public error!: string;
+  public taskData: any;
+  public title: string = 'Edit Task';
+
+
+  constructor(
+    private dataSource: RestDataSource,
+    private dialog: MatDialog,
+    private dialogRef: MatDialogRef<ChangeTaskComponent>,
+    @Inject(MAT_DIALOG_DATA) public dialogDataTask: any
+  ) { }
 
   ngOnInit(): void {
+    this.formGroup.patchValue(this.dialogDataTask);
+  }
+
+  editTask() {
+
+    this.dialogRef.close(this.formGroup.value);
+    this.task = this.formGroup.value;
+    console.log('Activity Category on form information -',this.task);
+    this.dataSource.editTask(this.dialogDataTask.id, this.dialogDataTask.title, this.dialogDataTask.description).subscribe(success => {
+      if (success) {
+        this.dialog.open(ChangesSavedComponent);
+
+      }
+    },
+      error => {
+        this.error = 'Transaction failed, changes not saved';
+        alert(this.error);
+        this.isLoading = false;
+      },
+
+    );
+
   }
 
 }
