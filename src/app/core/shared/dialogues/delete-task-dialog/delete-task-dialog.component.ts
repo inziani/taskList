ac15b052854512a82303a1b57b090bca36cea2cd
“inziani” <ValentineRobai.Inziani@gmail.com>
2022-03-09 10:51:31 +0300

    Complete deletion dialogue

--- src/app/core/shared/dialogues/delete-task-dialog/delete-task-dialog.component.ts
+++ src/app/core/shared/dialogues/delete-task-dialog/delete-task-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RestDataSource } from '../../data/rest.datasource';
+import { Tasks } from '../../models/task-model';
+import { ChangesSavedComponent } from '../changes-saved/changes-saved.component';
 
 @Component({
   selector: 'app-delete-task-dialog',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteTaskDialogComponent implements OnInit {
 
-  constructor() { }
+  public title: string = 'Delete Task';
+  public error!: string;
+  public isLoading: boolean = false;
+  public task!: Tasks;
+ 
+
+  constructor(
+
+    private dataSource: RestDataSource,
+    private dialog: MatDialog,
+    private dialogRef: MatDialogRef<DeleteTaskDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public deleteTaskDialogData: any,
+
+  ) { }
 
   ngOnInit(): void {
+
+    this.task = this.deleteTaskDialogData;
+  }
+
+  onDeleteTask() {
+
+    this.dataSource.deleteTask(this.task.id).subscribe(success => {
+      if (success) {
+        this.dialog.open(ChangesSavedComponent);
+      }
+
+    },
+      error => {
+        this.error = 'The deletion failed';
+        alert(this.error)
+        this.isLoading = false
+      }
+    )
+
   }
 
 }
--- src/app/core/shared/models/task-model.ts
+++ src/app/core/shared/models/task-model.ts
@@ -1,6 +1,7 @@
 export class Tasks{
 
   constructor(
+    public id: number,
     public title: string,
     public description: string
 
